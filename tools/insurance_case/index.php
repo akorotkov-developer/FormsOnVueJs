93e86a7868ba72c78b7a511e00d0bf0ed773a744
Александр Коротков <alex.korotkov@ylab.io>
2020-08-22 19:46:19 +0300

    init commit new template

--- tools/insurance_case/index.php
+++ tools/insurance_case/index.php
@@ -2,6 +2,11 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Формы");
 ?>
+<!-- Подключение Vue.js-->
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
+<div id="app">
+
+</div>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
